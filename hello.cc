81c7d17c0698fc6a47c77d52a08b50e3edee52ed
she <279180789@qq.com>
2016-04-03 21:54:18 +0800

    initial program

---
+++ hello.cc
@@ -0,0 +1,9 @@
+//time 3/4/2016
+//author she
+
+#include<iostream>
+int main(void)
+{
+	std::cout<<"hello world"<<std::endl;
+	return 0;
+}
---
+++ multiply.cpp
@@ -0,0 +1,13 @@
+//time 3/4/2016
+//author she
+//multiply A and B
+
+#include<iostream>
+int main(void)
+{
+	std::cout<<"please input two number:"<<std::endl;
+	int a,b;
+	std::cin>>a>>b;
+	std::cout<<"multiply "<<a<<" by "<<b<<" is "<<a*b<<std::endl;
+	return 0;
+}
